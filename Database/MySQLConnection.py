d4e51de80f0c413d6837585035e270d0720de533
Vasco Oliveira <vasco.oliveira260@gmail.com>
2023-05-23 19:53:24 +0200

    Update MySQLConnection.py

--- Database/MySQLConnection.py
+++ Database/MySQLConnection.py
@@ -46,13 +46,13 @@ def get_secret():
 
 @contextmanager
 def my_sql_connection():
-    #secret = get_secret()
-    #secret_json = json.loads(secret)
+    secret = get_secret()
+    secret_json = json.loads(secret)
 
     mydb = mysql.connector.connect(
-        host= 'localhost', #"personalcrm.c6l5guiieo6w.eu-west-3.rds.amazonaws.com",
-        user= 'root', #secret_json['username'],
-        password= 'bigdata' #secret_json['password']
+        host= "personalcrm.c6l5guiieo6w.eu-west-3.rds.amazonaws.com", #'localhost',
+        user= secret_json['username'], #'root', 
+        password= secret_json['password'] #'bigdata'
     )
     c = mydb.cursor()
     try:
